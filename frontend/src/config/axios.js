edaed6402939828270f59b5682595eee572a2b78
MehrSam <147476857+irkids@users.noreply.github.com>
2025-01-29 13:49:30 +0330

    Create axios.js

---
+++ frontend/src/config/axios.js
@@ -0,0 +1,33 @@
+// src/config/axios.js
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: '/',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+});
+
+instance.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    error => Promise.reject(error)
+);
+
+instance.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('token');
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default instance;
