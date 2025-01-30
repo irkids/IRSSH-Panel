71e21e261061d4289554469465b1f1d7ee32235b
MehrSam <147476857+irkids@users.noreply.github.com>
2025-01-30 05:51:10 +0330

    Create MainLayout.js

---
+++ frontend/src/layouts/MainLayout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 overflow-auto">
+        <main className="p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
