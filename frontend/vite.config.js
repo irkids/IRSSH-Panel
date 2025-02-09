e55454389a4c1bfbf171ce09c7ffaa5ce430a219
MehrSam <147476857+irkids@users.noreply.github.com>
2025-02-09 21:59:47 +0330

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  root: ".",
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: "index.html",
-    },
-  },
+  root: '/opt/irssh-panel/frontend',
+  publicDir: 'public',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    open: true,
+  }
 });
