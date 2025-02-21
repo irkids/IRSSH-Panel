144d38f5b10801359d9a56e0a7503da5a5083c1e
MehrSam <147476857+irkids@users.noreply.github.com>
2025-02-21 12:04:15 +0330

    Update main.tf

--- docs/terraform/main.tf
+++ docs/terraform/main.tf
@@ -40,4 +40,38 @@ module "elasticache" {
   source = "./modules/elasticache"
   
   subnet_ids = module.vpc.private_subnet_ids
-  security_group
+  security_group_ids = [module.security_groups.cache_sg_id]
+  node_type = var.cache_node_type
+  environment = var.environment
+}
+
+module "alb" {
+  source = "./modules/alb"
+  
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnet_ids
+  security_group_ids = [module.security_groups.alb_sg_id]
+  environment = var.environment
+}
+
+module "route53" {
+  source = "./modules/route53"
+  
+  domain_name = var.domain_name
+  alb_dns_name = module.alb.dns_name
+  alb_zone_id = module.alb.zone_id
+}
+
+module "s3" {
+  source = "./modules/s3"
+  
+  bucket_name = var.backup_bucket_name
+  environment = var.environment
+}
+
+module "cloudwatch" {
+  source = "./modules/cloudwatch"
+  
+  environment = var.environment
+  alarm_actions = var.alarm_actions
+}
