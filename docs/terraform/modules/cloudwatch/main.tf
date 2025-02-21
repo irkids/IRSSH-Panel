004dcf1bd8cb9ceed20cad378ee6cae7f8111736
MehrSam <147476857+irkids@users.noreply.github.com>
2025-02-21 12:24:21 +0330

    Create main.tf

---
+++ docs/terraform/modules/cloudwatch/main.tf
@@ -0,0 +1,24 @@
+resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
+  alarm_name          = "${var.project}-${var.environment}-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "This metric monitors EC2 CPU utilization"
+  alarm_actions       = var.alarm_actions
+
+  dimensions = {
+    AutoScalingGroupName = var.asg_name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "memory_utilization" {
+  alarm_name          = "${var.project}-${var.environment}-high-memory"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "MemoryUtilization"
+  namespace           = "System/Linux"
+  period              = "300
