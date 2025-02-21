0d321f955ab7848efe4ad08e47601ac1bb960b53
MehrSam <147476857+irkids@users.noreply.github.com>
2025-02-21 12:17:41 +0330

    Create main.tf

---
+++ docs/terraform/modules/alb/main.tf
@@ -0,0 +1,50 @@
+resource "aws_lb" "main" {
+  name               = "${var.project}-${var.environment}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = var.security_group_ids
+  subnets           = var.subnet_ids
+
+  enable_deletion_protection = var.environment == "production"
+
+  access_logs {
+    bucket  = var.log_bucket
+    prefix  = "alb-logs"
+    enabled = true
+  }
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-alb"
+    Environment = var.environment
+    Project     = var.project
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
