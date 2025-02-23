7372d2b9cc2721fc069261ee33c456297da63b3f
MehrSam <147476857+irkids@users.noreply.github.com>
2025-02-23 21:25:07 +0330

    Update dropbear-script.sh

--- modules/dropbear-script.sh
+++ modules/dropbear-script.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-import sys
-try:
-    from dotenv import load_dotenv
-except ImportError:
-    import pip
-    pip.main(['install', 'python-dotenv'])
-    from dotenv import load_dotenv
-
-load_dotenv()
 
 # Ultra-High Super Advanced Dropbear Installer and Configurator Script
 # Enhanced Version with Dynamic Configuration and PostgreSQL Logging
